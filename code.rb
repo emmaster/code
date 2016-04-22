99ed976fc4ea24ab9b79334f083fb80a83ee66bf
Oleksiy Kolesnikov <fullstability@gmail.com>
2016-04-22 15:57:52 +0300

    Cool third encoding approach

--- code.rb
+++ code.rb
@@ -3,16 +3,18 @@ require 'digest'
 puts "Какую фразу зашифровать?"
 phrase = STDIN.gets.chomp
 
-puts "Какой способ шифрования (1–MD5, 2–SHA1)?"
+puts "Какой способ шифрования (1–MD5, 2–SHA1, 3–SHA2?"
 choice = STDIN.gets.chomp.to_i
 
-until choice == 1 || choice == 2
+until choice == 1 || choice == 2 || choice == 3
   puts "неправильный ввод, повторите"
   choice = STDIN.gets.chomp.to_i
 end
 
 if choice == 1
   puts Digest::MD5.hexdigest(phrase)
-else
+elsif choice == 2
   puts Digest::SHA1.hexdigest(phrase)
+else
+  puts Digest::SHA2.hexdigest(phrase)
 end
